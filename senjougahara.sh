205e7e79f3fb34cc27291a9c6050770f677e2a8b
Morgotth <morgotth0@gmail.com>
2015-05-22 11:15:37 +0200

    Senjougahara: travis auto complete

--- senjougahara.sh
+++ senjougahara.sh
@@ -11,3 +11,5 @@ source $HOME/.my/_osx.sh
 export ANDROID_HOME=/usr/local/opt/android-sdk
 export ANDROID_NDK_HOME=/usr/local/opt/android-ndk
 
+# added by travis gem
+[ -f /Users/morgotth/.travis/travis.sh ] && source /Users/morgotth/.travis/travis.sh
